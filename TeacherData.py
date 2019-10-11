407d81a7892d456fd78ade97925904991beba136
Felipe Gemmal <lipegemmal@hotmail.com>
2019-10-11 20:20:43 -0300

    Turning Data into dictionary

--- TeacherData.py
+++ TeacherData.py
@@ -1,12 +1,10 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
+import re
 import json
 
-Dicionario = {
-
-}
-
+Dicionario = dict(dict())
 
 
 def getTitle(url):
@@ -15,37 +13,61 @@ def getTitle(url):
     except HTTPError as e:
         return None
     try:
-        bsObj = BeautifulSoup(html.read(),"lxml")
-        title = bsObj.body.h2  
+        bsObj = BeautifulSoup(html.read(), "lxml")
+        title = bsObj.body.h2
     except AttributeError as e:
         return None
     return title
 
 
-def getGreen(url):
+def setNames(nameList):
+    for name in nameList:
+        print(name)
+        Dicionario[name.get_text()] = None
+
+
+def getNames(url):
     try:
         html = urlopen(url)
     except HTTPError as e:
         return None
     #try:
-    bsObj = BeautifulSoup(html.read(),"lxml")
-    nameList = bsObj.findAll("td", {"class" : "views-field views-field-title active"})
-    return nameList
+    bsObj = BeautifulSoup(html.read(), "lxml")
+    nameList = bsObj.findAll("td", {"class": "views-field views-field-title active"})
+    setNames(nameList)
+
+
+def getData(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    #try:
+    bsObj = BeautifulSoup(html.read(), "lxml")
+    classList = bsObj.findAll("td", {"class": "views-field views-field-title active"})
     
-    #for name in nameList:
-     #   print(name.get_text())
+    for name in classList:
+        nome = str(name.get_text())[1:]
+        link = re.search("/node/[0-9]*", str(name))
+        Dicionario[nome] = {}
+        Dicionario[nome]['Nome'] = nome
+        Dicionario[nome]['Link'] = "www.inf.ufg.br"+link.group() 
+    
+    return classList
 
 
-#title = getTitle("http://www.inf.ufg.br")
- 
-#if title == None:
-#    print("No title")
-#else:
-#    print(title)
+linkList = getData("http://www.inf.ufg.br/docentes")
 
-nameList = getGreen("http://www.inf.ufg.br/docentes")
-for name in nameList:
-        Dicionario[name.get_text()] = None
-        
-#print("bu")
-print(*Dicionario.keys())
+#for link in linkList:
+#        #print(str(link))
+#        x = re.search("/node/[0-9]*",str(link))
+#        print(x.group())
+#        print()
+
+#print(*Dicionario,sep="\n")
+#print(list(Dicionario))
+
+for tupla in Dicionario:
+    print (Dicionario[tupla])
+   #x =list(tupla.values())
+   # print(x)
